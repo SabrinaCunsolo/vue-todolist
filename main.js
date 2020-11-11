9cd0ddbc6a6b664d7e194071a477f17357fb9b8a
Sabrina <sabrinacunsolo85@gmail.com>
2020-11-11 14:39:07 +0100

    aggiungere nuovo todo ok, click su check e todo rimosso ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // definire una lista di todo e stamparli in pagina in un elenco
-// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo
-// al click su un item della lista, il todo viene rimosso
+// predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo --> OK
+// al click su un item della lista, il todo viene rimosso ---> OK
 // BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
 // Non pensate alla grafica all'inizio, ma quando le cose girano perfettamente date sfogo alla vostra creatività! :laptop_parrot:
 // Nome repo: vue-todolist
@@ -9,6 +9,7 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+
             // creo array di todo
             todoList: [
                 'Comprare il pane',
@@ -21,6 +22,15 @@ var app = new Vue(
             nuovoToDo: ''
         },
         methods: {
+            // aggiungo a lista nuovo to do
+            aggiungiToDo() {
+                this.todoList.push(this.nuovoToDo);
+                this.nuovoToDo = '';
+            },
+            // rimuovi to do da lista, uno alla volta, solo se ci clicchi su
+            rimuoviToDo(element) {
+              this.todoList.splice(element, 1);
+            },
 
         },
 
