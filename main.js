4247d3582bd1333f91980d8dbf6c04e2020bf990
Sabrina <sabrinacunsolo85@gmail.com>
2020-11-11 16:27:57 +0100

    finito bonus

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // definire una lista di todo e stamparli in pagina in un elenco
 // predisporre un input testuale per aggiungere un nuovo todo: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei todo --> OK
 // al click su un item della lista, il todo viene rimosso ---> OK
-// BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo
+// BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo todo ---> OK
 // Non pensate alla grafica all'inizio, ma quando le cose girano perfettamente date sfogo alla vostra creatività! :laptop_parrot:
 // Nome repo: vue-todolist
 
@@ -30,8 +30,13 @@ var app = new Vue(
             rimuoviToDo(todoList, index) {
               this.todoList.splice(index, 1);
             },
+            // bonus
+            submit() {
+                this.todoList.push(this.nuovoToDo);
+                this.nuovoToDo = '';
+            }
 
-        },
+        }
 
 
     }
